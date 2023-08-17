b2726aa2900386a0d6e04bb61330459750253758
Hindol19 <hindolbanerjee5@gmail.com>
2023-08-17 21:50:15 +0530

    Fixed contact form

--- src/Components/Contact/Contact.jsx
+++ src/Components/Contact/Contact.jsx
@@ -1,22 +1,45 @@
-import React from "react";
+import React, { useRef } from "react";
 import "./Contact.scss";
+import emailjs from "@emailjs/browser";
 function Contact() {
+  const form = useRef();
+
+  const sendEmail = (e) => {
+    e.preventDefault();
+
+    emailjs
+      .sendForm(
+        process.env.REACT_APP_SERVICE_ID,
+        process.env.REACT_APP_TEMPLATE_ID,
+        form.current,
+        process.env.REACT_APP_PUBLIC_KEY
+      )
+      .then(
+        (result) => {
+          console.log(result.text);
+        },
+        (error) => {
+          console.log(error.text);
+        }
+      );
+  };
+
   return (
     <div className="contact-container">
       <div className="contact-layout">
         <div className="contact-card">
           <h1 className="contact-head">Contact Me</h1>
-          <div className="contact-text">
+          <form ref={form} onSubmit={sendEmail} className="contact-text">
             <input
               type="text"
-              name="name"
+              name="user_name"
               id=""
               className="name"
               placeholder="Enter your name"
             />
             <input
               type="text"
-              name="email"
+              name="user_email"
               id=""
               className="email"
               placeholder="Enter your email"
@@ -29,8 +52,10 @@ function Contact() {
               className="message"
               placeholder="Enter your message"
             />
-            <button>Submit</button>
-          </div>
+            <button type="submit" value="Send">
+              Submit
+            </button>
+          </form>
         </div>
       </div>
     </div>
